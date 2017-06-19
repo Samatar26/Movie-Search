49e2e1096a4add8c220980ac65f1c201fe11de51
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-19 12:59:34 +0100

    Create searchmovies action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { FETCH_LATEST_MOVIES } from './types';
+import { FETCH_LATEST_MOVIES, FETCH_SEARCH_MOVIES } from './types';
 const ROOT_URL = 'http://localhost:4000';
 
 export function fetchLatestMovies() {
@@ -6,6 +6,15 @@ export function fetchLatestMovies() {
     fetch(`${ROOT_URL}/latest`, { method: 'GET' })
       .then(response => response.json())
       .then(data => dispatch({ type: FETCH_LATEST_MOVIES, payload: data }))
+      .catch(err => console.log(err));
+  };
+}
+
+export function fetchSearchMovies() {
+  return function(dispatch) {
+    fetch(`${ROOT_URL}/search`, { method: 'GET' })
+      .then(response => response.json())
+      .then(data => dispatch({ type: FETCH_SEARCH_MOVIES, payload: data }))
       .catch(err => console.log(err));
   };
 }
