4a8cc06e2e661ceb8af0d765f98bd6cf312c7ce8
Nimo Axmed <samatar_267@hotmail.com>
2017-06-19 02:27:07 +0100

    Add Travis for CI

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Beginning of app.
+      </div>
+    );
+  }
+}
+
+export default App;
