a959c2db2c0117f87a79ca401e3ce20f7764a0c2
Nimo Axmed <samatar_267@hotmail.com>
2017-06-19 03:10:41 +0100

    Styled input field

--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { SearchBar } from './../styles/header';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class SearchForm extends Component {
   renderField = field => {
-    return <input type="text" {...field.input} />;
+    return <SearchBar type="text" {...field.input} />;
   };
   render() {
     return (
-      <div><SearchBar name="searchTerm" component={this.renderField} /></div>
+      <form>
+        <Field name="searchTerm" component={this.renderField} />
+      </form>
     );
   }
 }
--- src/styles/header.js
+++ src/styles/header.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 import { Field } from 'redux-form';
-export const SearchBar = styled(Field)`
 
+export const SearchBar = styled.input`
+  width: 100%;
+  padding: 0.75em 1em;
+  margin-right: 0.4em;
+  background-color: #ebebe2;
+  border-radius: 2px;
+  border: none;
+  font-size: inherit;
+  transition: background-color 0.2s;
+  transition: box-shadow 0.2s;
+  &:focus{
+    outline: none;
+    box-shadow: 0 0 0 2px palevioletred;
+  }
 `;
